4f1af6286e3585dff919d7ede2bc146ec19e2176
jamie7893 <jamie7893@live.com>
2015-11-16 05:11:09 +0000

    studies

--- projects/matchy/my_code.js
+++ projects/matchy/my_code.js
@@ -3,7 +3,7 @@ var animal = {};
 animal.species = "Lion";
 animal["name"] = "Shaggy";
 animal.noises = [];
-//console.log(animal);
+
 
 ////////
 var noises = [];
@@ -11,9 +11,33 @@ noises["noises"] = "Rawr";
 noises.push("Meow");
 noises.unshift("RAWWWRRR");
 noises[noises.length] =  "Rawrrrrrrr";
-//console.log(noises);
+
 
 // step 3
-animal.prop = noises;
+animal.noises = noises;
 noises.push("Meowwww");
-console.log(animal);
+
+
+
+//step 6
+
+var animals = [];
+animals.push(animal);
+var duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+animals.push(duck);
+var cat = { species: 'cat', name: 'Kitty Cat', noises: ['meoww', 'meow', 'sneeze', 'puuur'] };
+var dog = { species: 'dog', name: 'Timmy', noises: ['ruff', 'barkk', 'sneeze', 'growll'] };
+animals.push(cat);
+animals.push(dog);
+
+
+// I used a object because you it groups infomation together better. A Array is just a ordered pair
+var friends = {
+        randomAnimal: function() {
+            var random = animals(Math.random() * animals.length);
+            return random;
+        }
+    };
+    
+    
+console.log(friends.randomAnimal);
